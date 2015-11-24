7d45d18b2b9077e74938264720249cf03c6bbf3b
Christopher Hicks <chicks.net@gmail.com>
2015-11-24 13:31:51 -0800

    rest of Stanley message :dart:

--- The_Stanley_Parable/decode.py
+++ The_Stanley_Parable/decode.py
@@ -6,6 +6,7 @@ numbers = []
 with open('number_message.txt') as msg:
 	for line in msg:
 		tokens = line.split()
+		#print "line had " + str(len(tokens)) + " items"
 		for tok in tokens:
 			numbers.append(int(tok))
 
@@ -14,5 +15,6 @@ print "got " + str(len(numbers)) + " numbers"
 characters = []
 for num in numbers:
 	characters.append(str(chr(num)))
+	#print str(num) + ' -> ' + str(chr(num))
 
 print "".join(characters)
